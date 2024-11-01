79e05c784440740f5bc2e53dffc961ce90d74857
Mark Lallego <lallegomark@gmail.com>
2024-11-01 15:44:56 +0800

    New Python Repo

---
+++ Python Basics/main.py
@@ -0,0 +1,9 @@
+print("Hello, World!\n")
+
+name = input("What is your name? ")
+birth_year = input("When were you born? ")
+
+age = 2024 - int(birth_year)
+
+
+print(f"Hello, {name}!\nYou were born in {birth_year}.\nThat mean you're {age} years old\n")
